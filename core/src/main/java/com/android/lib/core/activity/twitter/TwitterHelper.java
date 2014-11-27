355346837e2dfd2db3bc2cf2d990830a812078ef
unknown <tgioihan@gmail.com>
2014-11-27 13:58:49 +0700

    cm

---
+++ core/src/main/java/com/android/lib/core/activity/twitter/TwitterHelper.java
@@ -0,0 +1,15 @@
+package com.android.lib.core.activity.twitter;
+
+import android.app.Activity;
+
+/**
+ * Created by tuannx on 11/27/2014.
+ */
+public class TwitterHelper {
+
+    private Activity context;
+
+    public TwitterHelper(Activity context) {
+        this.context = context;
+    }
+}
